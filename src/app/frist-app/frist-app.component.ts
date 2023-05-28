81e733c0783ab0baf9fe6b7f9d5a26e18c86eb97
M-Ziyed <134716475+M-Ziyed@users.noreply.github.com>
2023-05-28 17:33:42 +0300

    Add files via upload

---
+++ src/app/frist-app/frist-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'frist-app',
+  templateUrl: './frist-app.component.html',
+  styleUrls: ['./frist-app.component.css']
+})
+export class FristAppComponent {
+courses: string[] = ['C#' , 'Jave' , 'pyhan'];
+}
